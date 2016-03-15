8938d44c266aa038bb2db46aaeb79625c34fcb0d
Pablo <chispalex89@gmail.com>
2016-03-15 11:42:01 -0600

    Modified: Example Feature and Tools

--- OsiguSDK.SpecificationTests/Tools.cs
+++ OsiguSDK.SpecificationTests/Tools.cs
@@ -28,5 +28,35 @@ namespace OsiguSDK.SpecificationTests
         } }
 
         private static IConfiguration _configProvidersSandbox;
+
+        public static IConfiguration ConfigInsurersDevelopment
+        {
+            get
+            {
+                return _configInsurersDevelopment ?? (_configInsurersDevelopment = new Configuration
+                {
+                    BaseUrl = "https://dev.paycodenetwork.com",
+                    Slug = "test-insurer",
+                    Authentication = new Authentication("589a4586628aac2815d20c1e17bc11ab")
+                });
+            }
+        }
+
+        private static IConfiguration _configInsurersDevelopment;
+
+        public static IConfiguration ConfigProvidersDevelopment
+        {
+            get
+            {
+                return _configProvidersDevelopment ?? (_configProvidersDevelopment = new Configuration
+                {
+                    BaseUrl = "https://dev.paycodenetwork.com",
+                    Slug = "fayco",
+                    Authentication = new Authentication("589a4586628aac2815d20c1e17bc11ab")
+                });
+            }
+        }
+
+        private static IConfiguration _configProvidersDevelopment;
     }
 }
