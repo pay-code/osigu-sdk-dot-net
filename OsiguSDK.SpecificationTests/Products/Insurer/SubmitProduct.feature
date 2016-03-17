aa20405caed39703f45f2054e41e23fd30e6f6b0
Pablo <luisfr87@gmail.com>
2016-03-17 15:37:53 -0600

    Adding steps and future to test the Submit a product to complete the tests of getting a product

---
+++ OsiguSDK.SpecificationTests/Products/Insurer/SubmitProduct.feature
@@ -0,0 +1,21 @@
+﻿Feature: SubmitProduct as an Insurer
+	In order to add a product
+	As an Insurer
+	I want to send an authorized request to associate and add the product
+
+
+Scenario: Submit a request with an invalid token
+	Given I have the insurer products client with an invalid token
+	When I make the get list of products request to the endpoint
+	Then the result should be unauthorized for get a list of products
+
+Scenario: Submit a request with an invalid slug
+	Given I have the insurer products client with an invalid slug
+	When I make the get list of products request to the endpoint
+	Then the result should be access denied for get a list of products
+
+Scenario: Submit a new valid product
+	Given I have the insurer products client
+	And I have the request data for a new product
+	When I make the add a product insurer request to the endpoint
+	Then the response should be ok with code 204
---
+++ OsiguSDK.SpecificationTests/Products/Insurer/SubmitProductSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAssertions;
+using OsiguSDK.Insurers.Models;
+using OsiguSDK.Insurers.Models.Requests;
+using Ploeh.AutoFixture;
+using TechTalk.SpecFlow;
+
+namespace OsiguSDK.SpecificationTests.Products.Insurer
+{
+    [Binding]
+    public class SubmitProductAsAnInsurerSteps
+    {
+        private string errorMessage { get; set; }
+        private Product productResponse { get; set; }
+
+        [Given(@"I have the request data for a new product")]
+        public void GivenIHaveTheRequestDataForANewProduct()
+        {
+            try
+            {
+                Tools.submitInsurerProductRequest = Tools.Fixture.Create<SubmitProductRequest>();
+            }
+            catch (Exception exception)
+            {
+                errorMessage = exception.Message;
+            }
+        }
+        
+        [When(@"I make the add a product insurer request to the endpoint")]
+        public void WhenIMakeTheAddAProductInsurerRequestToTheEndpoint()
+        {
+            try
+            {
+                // La respuesta es correcta?
+                productResponse = Tools.productsInsurerClient.SubmitProduct(Tools.submitInsurerProductRequest);
+            }
+            catch (Exception exception)
+            {
+                errorMessage = exception.Message;
+            }
+        }
+
+        [Then(@"the response should be ok with code (.*)")]
+        public void ThenTheResponseShouldBeOkWithCode(int p0)
+        {
+            productResponse.ProductId.ShouldBeEquivalentTo(p0);
+        }
+
+    }
+}
