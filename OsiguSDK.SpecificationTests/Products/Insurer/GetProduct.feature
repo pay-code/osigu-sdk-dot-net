dba2d630f9b341b92cbdae435f2cf5a7ce8ef5db
Pablo <luisfr87@gmail.com>
2016-03-17 11:32:39 -0600

    Added steps and features for Get a Product for the insurer

---
+++ OsiguSDK.SpecificationTests/Products/Insurer/GetProduct.feature
@@ -0,0 +1,29 @@
+﻿Feature: Getting a product as an Insurer
+	In order to get information about a product
+	As an Insurer
+	I want to send an authorized request to get it
+
+Scenario: Submit a request with an invalid token
+	Given I have the insurer products client with an invalid token
+	When I make the get a product request to the endpoint
+	Then the result should be unauthorized for getting a product
+
+Scenario: Submit a request with an invalid slug
+	Given I have the insurer products client with an invalid slug
+	When I make the get a product request to the endpoint
+	Then the result should be access denied for getting a product
+
+Scenario: Submit a request with an invalid product id
+	Given I have the insurer products client
+	And I have an invalid product id
+	When I make the get a product request to the endpoint
+	Then the result should be ok
+	And a message error because the product does not exist
+
+Scenario: Submit a request with a valid product id
+	Given I have the insurer products client
+	And I have the request data for a new product
+	When I make the add a product insurer request to the endpoint
+	Then the response should be ok with code 204
+	When I make the get a product request to the endpoint
+	Then the result should be the product information
---
+++ OsiguSDK.SpecificationTests/Products/Insurer/GetProductSteps.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using FluentAssertions;
+using OsiguSDK.Insurers.Clients;
+using OsiguSDK.Insurers.Models;
+using OsiguSDK.Insurers.Models.Requests;
+using Ploeh.AutoFixture;
+using TechTalk.SpecFlow;
+
+namespace OsiguSDK.SpecificationTests.Products.Insurer
+{
+    [Binding]
+    public class GetProductSteps
+    {
+        private Product _responseProduct { get; set; }
+        private string errorMessage { get; set; }
+
+        [Given(@"I have an invalid product id")]
+        public void GivenIHaveAnInvalidProductId()
+        {
+            try
+            {
+                Tools.submitInsurerProductRequest = Tools.Fixture.Create<SubmitProductRequest>();
+            }
+            catch (Exception exception)
+            {
+                errorMessage = exception.Message;
+            }
+        }
+        
+        [When(@"I make the get a product request to the endpoint")]
+        public void WhenIMakeTheGetAProductRequestToTheEndpoint()
+        {
+            try
+            {
+                _responseProduct = Tools.productsInsurerClient.GetSingleProduct(Tools.submitInsurerProductRequest.ProductId);
+            }
+            catch (Exception exception)
+            {
+                errorMessage = exception.Message;
+            }
+            
+        }
+        
+        [Then(@"the result should be unauthorized for getting a product")]
+        public void ThenTheResultShouldBeUnauthorizedForGettingAProduct()
+        {
+            errorMessage.Should().Contain("Server failed to authenticate the request. Make sure the value of the Authorization header is formed correctly including the signature");
+        }
+
+        [Then(@"the result should be access denied for getting a product")]
+        public void ThenTheResultShouldBeAccessDeniedForGettingAProduct()
+        {
+            errorMessage.Should().Contain("You don’t have permission to access this resource");
+        }
+
+        [Then(@"a message error because the product does not exist")]
+        public void ThenAMessageErrorBecauseTheProductDoesNotExist()
+        {
+            errorMessage.Should().Contain("Not Found");
+        }
+
+        [Then(@"the result should be the product information")]
+        public void ThenTheResultShouldBeTheProductInformation()
+        {
+            _responseProduct.Should().NotBeNull();
+            _responseProduct.Should().Be(new Product(), " Expected a prodct information");
+            _responseProduct.Status.Should().Be("pending_review", "The product should not have been reviewed just yet");
+            _responseProduct.Type.Should().Be(Tools.submitInsurerProductRequest.Type);
+        }
+
+    }
+}
