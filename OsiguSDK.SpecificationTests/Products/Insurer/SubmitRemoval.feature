4fa683c1043c8a20144daba7ec24ffd9b35b8b2d
Pablo <luisfr87@gmail.com>
2016-03-17 15:38:48 -0600

    Adding the steps and features to test the remove a product for an insurer

---
+++ OsiguSDK.SpecificationTests/Products/Insurer/SubmitRemoval.feature
@@ -0,0 +1,38 @@
+﻿Feature: Submit product removal as an Insurer
+	In order to remove a product
+	As an Insurer
+	I want to send an authorized request to remove it
+
+Scenario: Request remove a product with an invalid token
+	Given I have the insurer products client with an invalid token
+	When I make the remove a product request to the endpoint
+	Then the result should be unauthorized for removing a product
+
+Scenario: Request remove a product with an invalid slug
+	Given I have the insurer products client with an invalid slug
+	When I make the remove a product request to the endpoint
+	Then the result should be access denied for removing a product
+
+Scenario: Request remove an unregistered product
+	Given I have the insurer products client
+	And I have an unregistered product information
+	When I make the remove a product request to the endpoint
+	Then the response should be 404 with product not found
+
+Scenario: Request remove a product with an invalid status
+	Given I have the insurer products client
+	And I have the request data for a new product
+	When I make the add a product insurer request to the endpoint
+	Then i have a 200 response of adding that product
+	When I make the remove a product request to the endpoint
+	Then the response should be ok with code 204 
+	When I make the remove a product request to the endpoint
+	Then the response should be ok with code 404 
+
+Scenario: Request remove a valid product
+	Given I have the insurer products client
+	And I have the request data for a new product
+	When I make the add a product insurer request to the endpoint
+	Then i have a 200 response of adding that product
+	When I make the remove a product request to the endpoint
+	Then the response should be ok with code 204 
---
+++ OsiguSDK.SpecificationTests/Products/Insurer/SubmitRemovalSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace OsiguSDK.SpecificationTests.Products.Insurer
+{
+    [Binding]
+    public class SubmitProductRemovalAsAnInsurerSteps
+    {
+        private string errorMessage { get; set; }
+
+        [Given(@"I have an unregistered product information")]
+        public void GivenIHaveAnUnregisteredProductInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I make the remove a product request to the endpoint")]
+        public void WhenIMakeTheRemoveAProductRequestToTheEndpoint()
+        {
+            try
+            {
+                Tools.productsInsurerClient.SubmitRemoval(Tools.submitInsurerProductRequest.ProductId);
+            }
+            catch (Exception exception)
+            {
+                        
+            }
+            
+        }
+        
+        [Then(@"the result should be unauthorized for removing a product")]
+        public void ThenTheResultShouldBeUnauthorizedForRemovingAProduct()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be access denied for removing a product")]
+        public void ThenTheResultShouldBeAccessDeniedForRemovingAProduct()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response should be (.*) with product not found")]
+        public void ThenTheResponseShouldBeWithProductNotFound(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"i have a (.*) response of adding that product")]
+        public void ThenIHaveAResponseOfAddingThatProduct(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
