77108587e46339d4d4cf254aa85125d865b4752e
Pablo <luisfr87@gmail.com>
2016-04-01 10:18:06 -0600

    Created, Feature and steps to void an authorization

---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/VoidAuthorization.feature
@@ -0,0 +1,10 @@
+﻿Feature: Void an authorization as an Insurer
+	In order to void an authorization
+	As an Insurer 
+	I need to send the id of the authorization
+
+Scenario: Void an authorization with an invalid token
+	Given I have the insurer authorizations client with an invalid token
+	And I have the request data for a new authorization
+	When I make the void authorization request to the endpoint
+	Then the result should be forbidden for voiding the authorization
---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/VoidAuthorizationSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using OsiguSDK.Core.Exceptions;
+using TechTalk.SpecFlow;
+
+namespace OsiguSDK.SpecificationTests.Authorizations.Insurers
+{
+    [Binding]
+    public class VoidAnAuthorizationAsAnInsurerSteps
+    {
+        private RequestException errorMessage { get; set; }
+
+        [When(@"I make the void authorization request to the endpoint")]
+        public void WhenIMakeTheVoidAuthorizationRequestToTheEndpoint()
+        {
+            try
+            {
+                Tools.insurerAuthorizationClient.VoidAuthorization(Tools.AuthorizationId);
+                errorMessage = new RequestException("ok", 204);
+            }
+            catch (RequestException exception)
+            {
+                errorMessage = exception;
+            }
+        }
+        
+        [Then(@"the result should be forbidden for voiding the authorization")]
+        public void ThenTheResultShouldBeForbiddenForVoidingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(403);
+        }
+    }
+}
