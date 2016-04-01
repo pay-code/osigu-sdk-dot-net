8f744675eb80e6497d541d29e54c7de0cfd65232
Pablo <luisfr87@gmail.com>
2016-04-01 10:16:52 -0600

    Created, Steps and features for get an authorization

---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/GetAuthorization.feature
@@ -0,0 +1,30 @@
+﻿Feature: Get an authorization as an Insurer
+	In order to obtain an authorization
+	As an Insurer 
+	I need to send the id of the authorization
+
+Scenario: Get an authorization with an invalid token
+	Given I have the insurer authorizations client with an invalid token
+	And I have an invalid authorization id
+	When I make the get authorization request to the endpoint
+	Then the result should be forbidden for getting the authorization
+
+Scenario: Get an authorization with an invalid slug
+	Given I have the insurer authorizations client with an invalid slug
+	And I have an invalid authorization id
+	When I make the get authorization request to the endpoint
+	Then the result should be not found for getting the authorization
+
+Scenario: Get an authorization with an invalid authorization id
+	Given I have the insurer authorizations client
+	And I have an invalid authorization id
+	When I make the get authorization request to the endpoint
+	Then the result should be not found for getting the authorization
+
+Scenario: Get an authorization with a valid authorization id
+	Given I have the insurer authorizations client
+	And I have the request data for a new authorization
+	When I make the new authorization request to the endpoint
+	Then I have valid response for creating the authorization
+	And I make the get authorization request to the endpoint
+	Then I have a valid response for getting the authorization
---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/GetAuthorizationSteps.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using FluentAssertions;
+using OsiguSDK.Core.Exceptions;
+using OsiguSDK.Insurers.Models;
+using TechTalk.SpecFlow;
+
+namespace OsiguSDK.SpecificationTests.Authorizations.Insurers
+{
+    [Binding]
+    public class GetAnAuthorizationAsAnInsurerSteps
+    {
+        private RequestException errorMessage { get; set; }
+        private Authorization responseAuthorization { get; set; }
+
+        [When(@"I make the get authorization request to the endpoint")]
+        public void WhenIMakeTheGetAuthorizationRequestToTheEndpoint()
+        {
+            try
+            {
+                responseAuthorization = Tools.insurerAuthorizationClient.GetSingleAuthorization(Tools.AuthorizationId);
+                Tools.AuthorizationId = responseAuthorization.Id;
+                errorMessage = new RequestException("ok", 200);
+            }
+            catch (RequestException exception)
+            {
+                errorMessage = exception;
+            }
+        }
+
+        [Given(@"I have an invalid authorization id")]
+        public void GivenIHaveAnInvalidAuthorizationId()
+        {
+            Tools.AuthorizationId = "12345";
+        }
+
+
+        [Then(@"the result should be forbidden for getting the authorization")]
+        public void ThenTheResultShouldBeForbiddenForGettingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(403);
+        }
+
+        [Then(@"the result should be not found for getting the authorization")]
+        public void ThenTheResultShouldBeNotFoundForGettingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(404);
+        }
+
+        [Then(@"I make the get authorization request to the endpoint")]
+        public void ThenIMakeTheGetAuthorizationRequestToTheEndpoint()
+        {
+            try
+            {
+                responseAuthorization = Tools.insurerAuthorizationClient.GetSingleAuthorization(Tools.AuthorizationId);
+                Tools.AuthorizationId = responseAuthorization.Id;
+                errorMessage = new RequestException("ok", 200);
+            }
+            catch (RequestException exception)
+            {
+                errorMessage = exception;
+            }
+        }
+
+        [Then(@"I have a valid response for getting the authorization")]
+        public void ThenIHaveAValidResponseForGettingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(200);
+        }
+
+    }
+}
