21706a5d4ad76484ac9068a041ec498301e2b449
Pablo <luisfr87@gmail.com>
2016-04-01 10:17:25 -0600

    Created, Steps and features for updating an authorization

---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/UpdateAuthorization.feature
@@ -0,0 +1,30 @@
+﻿Feature: Update an authorization as an Insurer
+	In order to update an authorization
+	As an Insurer 
+	I need to send the updated data of the authorization
+
+Scenario: Update an authorization with an invalid token
+	Given I have the insurer authorizations client with an invalid token
+	And I have the request data for a new authorization
+	When I make the update authorization request to the endpoint
+	Then the result should be forbidden for updating the authorization
+
+Scenario: Update an authorization with an invalid slug
+	Given I have the insurer authorizations client with an invalid slug
+	And I have the request data for a new authorization
+	When I make the update authorization request to the endpoint
+	Then the result should be not found for updating the authorization
+
+Scenario: Update an authorization with an invalid authorization id
+	Given I have the insurer authorizations client
+	And I have the request data for a new authorization
+	When I make the update authorization request to the endpoint
+	Then the result should be not found for updating the authorization
+
+Scenario: Update an authorization with a valid authorization id
+	Given I have the insurer authorizations client
+	And I have the request data for a new authorization
+	When I make the new authorization request to the endpoint
+	Then I have valid response for creating the authorization
+	And I make the update authorization request to the endpoint
+	Then the result should be not found for updating the authorization
---
+++ OsiguSDK.SpecificationTests/Authorizations/Insurers/UpdateAuthorizationSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using OsiguSDK.Core.Exceptions;
+using TechTalk.SpecFlow;
+using OsiguSDK.Insurers.Models;
+
+namespace OsiguSDK.SpecificationTests.Authorizations.Insurers
+{
+    [Binding]
+    public class UpdateAnAuthorizationAsAnInsurerSteps
+    {
+        private RequestException errorMessage { get; set; }
+        private Authorization responseAuthorization { get; set; }
+        [When(@"I make the update authorization request to the endpoint")]
+        public void WhenIMakeTheUpdateAuthorizationRequestToTheEndpoint()
+        {
+            try
+            {
+                responseAuthorization = Tools.insurerAuthorizationClient.ModifyAuthorization(Tools.AuthorizationId,Tools.submitAuthorizationRequest);
+                Tools.AuthorizationId = responseAuthorization.Id;
+                errorMessage = new RequestException("ok", 200);
+            }
+            catch (RequestException exception)
+            {
+                errorMessage = exception;
+            }
+        }
+        
+        [Then(@"the result should be forbidden for updating the authorization")]
+        public void ThenTheResultShouldBeForbiddenForUpdatingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(403);
+        }
+
+        [Then(@"the result should be not found for updating the authorization")]
+        public void ThenTheResultShouldBeNotFoundForUpdatingTheAuthorization()
+        {
+            errorMessage.ResponseCode.Should().Be(404);
+        }
+
+    }
+}
