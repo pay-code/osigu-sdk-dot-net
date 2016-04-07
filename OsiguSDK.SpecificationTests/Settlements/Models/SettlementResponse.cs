8c6ecc189f2eadafffb89d693587a6c225370526
Pablo <chispalex89@gmail.com>
2016-04-07 10:31:29 -0600

    MODIFIED: Models

---
+++ OsiguSDK.SpecificationTests/Settlements/Models/SettlementResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace OsiguSDK.SpecificationTests.AuxiliarModels
+{
+    public class SettlementResponse
+    {
+        public int Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TotalDiscounts { get; set; }
+        public int InsurerId { get; set; }
+        public int ProviderId { get; set; }
+        public int ProviderCompanyId { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal RevenueShare { get; set; }
+    }
+}
