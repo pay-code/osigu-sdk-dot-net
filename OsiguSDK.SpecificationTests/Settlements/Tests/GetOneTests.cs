4131a2fc01f6f3859f5486f02b0639c52bdab217
Pablo <chispalex89@gmail.com>
2016-04-08 11:08:28 -0600

    [ADDED]: Settlements -> GetOne Tests

---
+++ OsiguSDK.SpecificationTests/Settlements/Tests/GetOneTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using NHibernate.Linq.Clauses;
+using NUnit.Framework;
+using OsiguSDK.Core.Config;
+using OsiguSDK.SpecificationTests.Settlements.Models;
+using RestSharp;
+
+namespace OsiguSDK.SpecificationTests.Settlements.Tests
+{
+    [TestFixture]
+    public class GetOneTests : BaseTest
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            Client = new RestClient(new Configuration
+            {
+                BaseUrl = Tools.BaseUrl
+            });
+        }
+
+
+        [Test]
+        public void ValidateAuthenticationError()
+        {
+
+        }
+
+        [Test]
+        public void ValidateInvalidSettlementId()
+        {
+
+        }
+
+        [Test]
+        public void ValidateSuccess()
+        {
+            var response = Client.RequestToEndpoint<SettlementResponse>(Method.GET, "/settlements/1");
+
+           
+        }
+    }
+}
