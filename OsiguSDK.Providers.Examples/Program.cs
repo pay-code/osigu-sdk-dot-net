af67b80198b3dd9ba51f2d88a3719e90525d9c4f
Fernando Segura <fernandosure@yahoo.com>
2016-05-12 11:33:32 -0600

    provider examples for sdk

--- OsiguSDK.Providers.Examples/Program.cs
+++ OsiguSDK.Providers.Examples/Program.cs
@@ -1,5 +1,8 @@
-﻿using OsiguSDK.Core.Config;
+﻿using System.Collections.Generic;
+using OsiguSDK.Core.Config;
 using OsiguSDK.Providers.Clients;
+using OsiguSDK.Providers.Models;
+using OsiguSDK.Providers.Models.Requests;
 
 namespace OsiguSDK.Providers.Examples
 {
@@ -12,11 +15,40 @@ namespace OsiguSDK.Providers.Examples
 
 
             //GET A SINGLE PRE-AUTHORIZATION
-            var authResult = _client.Authorizations.GetSingleAuthorization("GT-8KTA-ledu");
+            var testAuth = "123"
+            var authResult = _client.Authorizations.GetSingleAuthorization(testAuth);
             
 
-            //GET 
+            //CREATING A CLAIM
 
+            var claim = new CreateClaimRequest()
+            {
+                Pin = "123",
+                Items = new List<CreateClaimRequest.Item>()
+                {
+                    new CreateClaimRequest.Item()
+                    {
+                        ProductId = authResult.Items[0].ProductId,
+                        Quantity = authResult.Items[0].Quantity,
+                        Price = 100
+                    }
+                }
+            };
+
+
+            var claimResult = _client.Claims.CreateClaim(testAuth,claim);
+
+
+            //GET THE STATUS OF THE QUEUE
+            QueueStatus status = _client.Queue.CheckQueueStatus(claimResult);
+
+            while (string.IsNullOrEmpty(status.ResourceId))
+            {
+                status = _client.Queue.CheckQueueStatus(claimResult);
+            }
+            
+
+            
 
 
 
